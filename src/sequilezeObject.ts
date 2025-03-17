0e0a439466300f41ad21e4e3f6427d7c865a99b6
AnushaUppuu <anusha.uppu@everest.engineering>
2025-03-17 15:06:19 +0530

    Update the database

--- src/sequilezeObject.ts
+++ src/sequilezeObject.ts
@@ -6,6 +6,7 @@ const username = process.env.USERNAME ? process.env.USERNAME : "anusha";
 const database = process.env.DATABASE ? process.env.DATABASE : "testing";
 const password = process.env.PASSWORD ? process.env.PASSWORD : "anusha";
 const host=process.env.HOST ? process.env.HOST:"localhost";
+console.log('environment variables',username,database,password,host);
 export const sequelize = new Sequelize(database,username,
   password,
  {
